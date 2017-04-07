96f86fa66ece498b2f6e04ff0a848b390c58865e
Kirill Yakovenko <kirill.yakovenko@partner.hansoft.com>
2017-04-07 15:41:06 +0300

    [X] add lessons code

---
+++ lesson01.js
@@ -0,0 +1,10 @@
+//Lesson1 - Intro
+
+//  http://esprima.org/demo/parse.html#
+// treehouse.com (For code writing learning)
+
+sorax
+// refrence is not defined
+
+//string literal
+{name: "sorax"}
---
+++ lesson2/lesson2and3.js
@@ -0,0 +1,16 @@
+345 +456;
+ + 45;
+// self-invoking function
+(function(){
+
+}())
+
+console.log("Hellow World!");
+;
+var num;
+num = 1234;
+console.log(num);
+
+var num2;
+num2 = 12 + 34;
+console.log(num2);
